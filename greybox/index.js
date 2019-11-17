3af1c4da4a6e64a4658b5d98eb1dec3571247d2d
peyo <peteryyoon@gmail.com>
2019-11-17 01:09:43 -0800

    first commit

---
+++ greybox/index.js
@@ -0,0 +1,13 @@
+$$(document).ready(function() {
+    $(".question-and-score:empty").remove();
+    $(".title-content:empty").remove();
+    $(".question-type:empty").remove();
+    $(".question-explanation:empty").remove();
+    $(".question:empty").remove();
+    $(".choices:empty").remove();
+    $(".answer:empty").remove();
+    $(".answer-bg:empty").remove();
+    $(".correct-incorrect:emptyy").remove();
+    $(".answer-explanation:empty").remove();
+    $(".answer-explanation-bg:empty").remove();
+  });
